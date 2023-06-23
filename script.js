3175eb27724df0eecf7168d4f5748c03e43c14f6
Jayson Morilla <60346390+jaysonmorilla@users.noreply.github.com>
2023-06-23 03:25:57 -0700

    Add files via upload
    Added javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+
+	const _query = (__email = '') => {
+		document.querySelector(".modal-notification").style.display = 'block';
+		document.querySelector(".wrapper-flex").style.display = 'none';
+	}
+
+	const _dismiss = () => {
+		console.log(window.location);
+		window.location.replace(window.location.pathname);
+	}
+
+	const _request = e.target.location.search;
+	const _data = (_request).split('&');
+	const _email = (_request !== null || !isEmptyOrSpaces(_request)) ? _data[0].substr(7).replace('%40', '@') : undefined;
+
+	if(_email.length !== 0) {
+		_query(_email);
+	}
+
+	document.querySelector("._dismissBtn").addEventListener('click', _dismiss);
+});
